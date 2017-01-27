9c8e8defe74aaca7a0f461480564f84fa657a0e0
Nympheo <synthetic.probe@gmail.com>
2017-01-27 19:38:06 +0300

    doubly-linked-list/Features: added some methods

--- src/linked-list.js
+++ src/linked-list.js
@@ -2,20 +2,58 @@ const Node = require('./node');
 
 class LinkedList {
     constructor() {
-      1
+      this.data = data;
+      this.prev = prev;
+      this.next = next;
     }
 
-    append(data) {}
+    append(data) {
+      if(this.length === 0){
+        this._head = this._tail = new Node(data);
+      } else {
+        this._head.next = new Node(data,this._head);
+        this._head = this._head.next;
+      }
+      this.length++;
+    }
 
-    head() {}
+    head() {
+      return this._head.data;
+    }
 
-    tail() {}
+    tail() {
+      return this._tail.data;
+    }
 
-    at(index) {}
+    at(index) {
+      let temp = this._tail;
+      while(index > 0) {
+        temp = temp.next;
+        index--;
+      }
+      return temp.data;
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+      if(index === 0){
+        this._tail = new Node(data, null, this._tail);
+      } else {
+        let parent = this._tail;
+        while(index > 0) {
+          parent = parent.next;
+          index--;
+        }
+        parent = parent.prev;
+        let elem = new Node(data, parent, parent.next);
+        parent.next.prev = elem;
+        parent.next = elem;
+      }
+      this.length++;
+    }
 
-    isEmpty() {}
+    isEmpty() {
+      return this.length > 0 ? false : true;
+    }
 
     clear() {}
 
